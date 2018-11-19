9e32f970943bf5911315de4fe59d9f74d685304d
shenhuanjie <shenhuanjie@live.cn>
2018-11-19 21:05:40 +0800

    commit mysqltest.java

---
+++ src/main/java/springboot/example/entity/Department.java
@@ -0,0 +1,32 @@
+package springboot.example.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "deparment")
+public class Department {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    public  Department(){
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/springboot/example/repository/DepartmentRepository.java
@@ -0,0 +1,10 @@
+package springboot.example.repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import springboot.example.entity.Department;
+
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+}
